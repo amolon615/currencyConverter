601a7ba3032b2cdc2b628f67dcb97e587f86df75
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-06-01 13:58:54 +0200

    Initial Commit

---
+++ currencyConverter/currencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currencyConverterApp.swift
+//  currencyConverter
+//
+//  Created by Artem on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct currencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
